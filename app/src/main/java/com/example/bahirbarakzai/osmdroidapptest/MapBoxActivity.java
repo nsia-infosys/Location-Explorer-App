a97632d89db604c42b33f0ae419f461e07c5c4b4
Bahir Barakzai <m.bahir.barakzai@gmail.com>
2019-04-06 11:01:16 +0430

    adding Mapbox Activity

---
+++ app/src/main/java/com/example/bahirbarakzai/osmdroidapptest/MapBoxActivity.java
@@ -0,0 +1,39 @@
+package com.example.bahirbarakzai.osmdroidapptest;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class MapBoxActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_box);
+
+        Mapbox.getInstance(this,"sk.eyJ1IjoiYmFoaXJiYXJhayIsImEiOiJjanJweWxwbm8wYTNiM3ltcjVkYmx3YWE4In0.xK0aLiiTkkLbS5Io4MnpcQ");
+
+        mMapView = findViewById(R.id.mapview_mapbox);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+
+                mapboxMap.setStyle(Style.SATELLITE, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                    }
+                });
+            }
+        });
+    }
+}
